1161b772868c9ccfa9a1a1f96d556c4e691718c2
Samanvith <Samanvith2005@gmail.com>
2024-09-12 12:48:25 +0530

    used geolocation db to get the ip

--- src/app/(app)/ip-info/page.jsx
+++ src/app/(app)/ip-info/page.jsx
@@ -2,48 +2,47 @@
 import { useEffect, useState } from 'react';
 
 export default function GeolocationComponent() {
-  const [ip, setIp] = useState(null);
   const [location, setLocation] = useState(null);
+  console.log(location);
+  
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch the public IP address
-    const fetchIp = async () => {
+    const fetchLocation = async () => {
       try {
-        const response = await fetch('https://api.ipify.org?format=json');
+       
+        const response = await fetch('https://geolocation-db.com/json/e18b48c0-6cef-11ef-be25-fdc8f1201945');
         const data = await response.json();
-        setIp(data.ip);
+         
+         
+        if (data) {
+          setLocation(data);
+        } else {
+          setError("Failed to fetch location data.");
+        }
       } catch (error) {
-        console.error("Error fetching IP address:", error);
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data.");
       }
     };
 
-    fetchIp();
-
-    // Fetch the geolocation data
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error("Error retrieving location:", error);
-        }
-      );
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
+    fetchLocation();
   }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div>
       <h1>Geolocation and IP Address</h1>
       <div>
-        <strong>IP Address:</strong> {ip || 'Loading...'}
-      </div>
-      <div>
+        <strong>IP Address:</strong> {location?.IPv4 || 'Loading...'}<br />
+        <strong>City:</strong> {location?.city || 'Loading...'}<br />
+        <strong>State:</strong> {location?.state || 'Loading...'}<br />
+        <strong>Country:</strong> {location?.country_name || 'Loading...'}<br />
         <strong>Latitude:</strong> {location?.latitude || 'Loading...'}<br />
-        <strong>Longitude:</strong> {location?.longitude || 'Loading...'}
+        <strong>Longitude:</strong> {location?.longitude || 'Loading...'}<br />
       </div>
     </div>
   );
